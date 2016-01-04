1e6565bda526ef703bda7a994e00106775746988
Mike Trumbell <mtrumbell@optionscitys-MacBook-Pro.local>
2016-01-04 16:58:19 -0600

    Disabled poller and added button.  Also updated packages.

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,9 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  poller: Ember.inject.service('poller'),
-
-  init: function(){
-    this.get("poller").poll();
+  // poller: Ember.inject.service('poller'),
+  // init: function(){
+  //   this.get("poller").poll();
+  // },
+  actions: {
+    queryData: function(){
+      this.get("store").query("marketdatum", {}).then(function(records){
+        records.destroy();
+      });
+    }
   }
 });
--- app/services/poller.js
+++ app/services/poller.js
@@ -6,8 +6,7 @@ export default Ember.Service.extend({
   // poller setup for 800ms
   poll: function(){
     this.get("store").query("marketdatum", {}).then(function(records){
-      var size = records.get("firstObject").get('_internalModel._recordArrays.size');
-      console.log('_internalModel._recordArrays is now ' + size + ' items long.')
+      records.destroy();
     });
     Ember.run.later(this, this.poll.bind(this), 800);
   }
